1f583cb0a806c0c410e450f3c14b450ea70eed31
jpedro453 <jpedroswc453@gmail.com>
2024-01-10 17:28:44 -0300

    feat: test if signup returns an error if no password is passed

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -31,4 +31,17 @@ describe('SignUp Controller', () => {
         expect(httpResponse.statusCode).toBe(400);
         expect(httpResponse.body).toEqual(new MissingParamError('email'));
     });
+    test('Should return 400 if no name is provided', () => {
+        const sut = new SignUpController();
+        const httpRequest = {
+            body: {
+                name: 'name',
+                email: 'any_email@mail.com',
+                password_confirmation: 'pwd',
+            },
+        };
+        const httpResponse = sut.handle(httpRequest);
+        expect(httpResponse.statusCode).toBe(400);
+        expect(httpResponse.body).toEqual(new MissingParamError('password'));
+    });
 });
