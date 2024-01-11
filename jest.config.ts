afb3a9611f78e9896d970b892957ee029c7ca8fe
jpedro453 <jpedroswc453@gmail.com>
2024-01-11 20:05:54 -0300

    chore: jest configuration

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,7 @@
  * https://jestjs.io/docs/configuration
  */
 
-import type { Config } from 'jest';
+import type { Config } from 'jest'
 
 const config: Config = {
     // All imported modules in your tests should be mocked automatically
@@ -144,7 +144,7 @@ const config: Config = {
     // snapshotSerializers: [],
 
     // The test environment that will be used for testing
-    // testEnvironment: "jest-environment-node",
+    testEnvironment: 'node',
 
     // Options that will be passed to the testEnvironment
     // testEnvironmentOptions: {},
@@ -194,6 +194,6 @@ const config: Config = {
 
     // Whether to use watchman for file crawling
     // watchman: true,
-};
+}
 
-export default config;
+export default config
