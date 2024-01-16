fe166ed11c88ab8f9704ed911eb55c064c5e8d6a
jpedro453 <jpedroswc453@gmail.com>
2024-01-15 21:06:22 -0300

    refactor: mongodb methods in MongoHelper class

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -3,10 +3,10 @@ import { MongoClient } from 'mongodb'
 export const MongoHelper = {
     client: null as MongoClient,
 
-    async connect(uri: string) {
+    async connect(uri: string): Promise<void> {
         this.client = await MongoClient.connect(process.env.MONGO_URL)
     },
-    async disconnect() {
+    async disconnect(): Promise<void> {
         await this.client.close()
     },
 }
