11d41dd399652d95d3d7a6c5e5dac483892bd6c5
jpedro453 <jpedroswc453@gmail.com>
2024-01-17 22:18:57 -0300

    feat: ensure routes always responds with json

--- src/main/middlewares/cors.test.ts
+++ src/main/middlewares/cors.test.ts
@@ -1,7 +1,7 @@
 import request from 'supertest'
 import app from '../config/app'
 
-describe('Body Parser Middleware', () => {
+describe('CORS Middleware', () => {
     test('Should enable CORS', async () => {
         app.get('/test_cors', (req, res) => {
             res.send()
