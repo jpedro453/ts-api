c359f7482f7ba7126bf842b95f56687ba7e28717
jpedro453 <jpedroswc453@gmail.com>
2024-01-18 20:25:23 -0300

    refactor: ensure mongo is initialized before running sign up test

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,7 +1,19 @@
 import request from 'supertest'
 import app from '../config/app'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 
 describe('Signup routes', () => {
+    beforeAll(async () => {
+        await MongoHelper.connect('mongodb://127.0.0.1:27017/node-api')
+    }, 5000)
+    afterAll(async () => {
+        await MongoHelper.disconnect()
+    })
+
+    beforeEach(async () => {
+        const accountCollection = await MongoHelper.getCollection('accounts')
+        await accountCollection.deleteMany({})
+    })
     test('Should return an account on success', async () => {
         await request(app)
             .post('/api/signup')
