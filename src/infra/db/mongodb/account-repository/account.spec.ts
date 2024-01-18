097d4716456eb32a954eae9169811f8a80732263
jpedro453 <jpedroswc453@gmail.com>
2024-01-17 21:22:32 -0300

    test: AccountMongoRepository returns an account on success

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -1,19 +1,20 @@
 import { MongoHelper } from '../helpers/mongo-helper'
+import { AccountMongoRepository } from './account'
 
 describe('Account Mongo Repository', () => {
     beforeAll(async () => {
-        await MongoHelper.connect(process.env.MONGO_URL)
-    })
+        await MongoHelper.connect('mongodb://127.0.0.1:27017/node-api')
+    }, 5000)
     afterAll(async () => {
         await MongoHelper.disconnect()
     })
 
-    test('should return an account on succes', async () => {
+    it('should return an account on succes', async () => {
         const sut = new AccountMongoRepository()
         const account = await sut.add({
             name: 'any_name',
             email: 'any_email@email.com',
-            password: 'any_password',
+            password: 'any_password'
         })
         expect(account).toBeTruthy()
         expect(account.id).toBeTruthy()
