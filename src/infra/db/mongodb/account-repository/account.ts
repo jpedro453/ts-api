66887b46e142d7c8ae9bec58e48167e8445dd8f4
jpedro453 <jpedroswc453@gmail.com>
2024-01-17 21:22:21 -0300

    feat: AccountMongoRepository returns an account on success

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -1,1 +1,15 @@
+import { IAddAccountRepository } from '../../../../data/protocols/add-account-repository'
+import { IAccountModel } from '../../../../domain/models/account'
+import { IAddAccountModel } from '../../../../domain/useCases/add-account'
+import { MongoHelper } from '../helpers/mongo-helper'
 
+export class AccountMongoRepository implements IAddAccountRepository {
+    async add(accountData: IAddAccountModel): Promise<IAccountModel> {
+        const accountCollection = await MongoHelper.getCollection('accounts')
+        const result = await accountCollection.insertOne(accountData)
+        const account = await MongoHelper.getCollectionItemById('accounts', result.insertedId)
+        const { _id, ...accountWithoutId } = account
+
+        return Object.assign({}, accountWithoutId, { id: _id })
+    }
+}
--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,12 +1,24 @@
-import { MongoClient } from 'mongodb'
+import { MongoClient, Collection } from 'mongodb'
+import * as dotenv from 'dotenv'
+dotenv.config({ path: __dirname + '/.env' })
 
 export const MongoHelper = {
     client: null as MongoClient,
 
     async connect(uri: string): Promise<void> {
-        this.client = await MongoClient.connect(process.env.MONGO_URL)
+        this.client = await MongoClient.connect(uri)
     },
     async disconnect(): Promise<void> {
-        await this.client.close()
+        if (this.client) {
+            await this.client.close()
+        }
     },
+
+    async getCollection(name: string): Promise<Collection> {
+        return this.client.db().collection(name)
+    },
+    // FIXME: TEMPORÁRIO, mongodb não aceita mais o .ops apos o insertOne
+    async getCollectionItemById(collection: string, id: any): Promise<any> {
+        return this.client.db().collection(collection).findOne({ _id: id })
+    }
 }
