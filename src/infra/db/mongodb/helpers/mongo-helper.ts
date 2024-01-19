0325816321cb30eaf4bbd4f4f8b98a744e901e00
jpedro453 <jpedroswc453@gmail.com>
2024-01-19 18:55:05 -0300

    refactor: ensure mongo client is connected before executing queries

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -2,14 +2,17 @@ import { MongoClient, Collection } from 'mongodb'
 
 export const MongoHelper = {
     client: null as MongoClient,
+    uri: null as string,
 
     async connect(uri: string): Promise<void> {
+        this.uri = uri
         this.client = await MongoClient.connect(uri)
     },
 
     async disconnect(): Promise<void> {
         if (this.client) {
             await this.client.close()
+            this.client = null
         }
     },
 
@@ -20,11 +23,17 @@ export const MongoHelper = {
     },
 
     async getCollection(name: string): Promise<Collection> {
+        if (this.client == null) {
+            await this.connect(this.uri)
+        }
         return this.client.db().collection(name)
     },
 
     // FIXME: TEMPORÁRIO: mongodb não aceita mais o .ops apos o insertOne
     async getCollectionItemById(collection: string, id: any): Promise<any> {
+        if (this.client == null) {
+            await this.connect(this.uri)
+        }
         return this.client.db().collection(collection).findOne({ _id: id })
     }
 }
