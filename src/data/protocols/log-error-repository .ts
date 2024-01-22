956a6aeb9d7907f504163e8ead22a48f68226d7f
jpedro453 <jpedroswc453@gmail.com>
2024-01-21 21:31:52 -0300

    feat: ensure logMongoRepository create a error log

--- src/data/protocols/log-error-repository .ts
+++ src/data/protocols/log-error-repository .ts
@@ -1,5 +1,5 @@
 import { IAccountModel, IAddAccountModel } from '../useCases/add-account/db-add-account-protocols'
 
 export interface ILogErrorRepository {
-    log(stack: string): Promise<void>
+    logError(stack: string): Promise<void>
 }
