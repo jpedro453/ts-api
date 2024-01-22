49b1fe548c9d6daa1e40456142578c684adf8633
jpedro453 <jpedroswc453@gmail.com>
2024-01-21 21:48:50 -0300

    refactor: account repository with correct values

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -6,7 +6,7 @@ import { MongoHelper } from '../helpers/mongo-helper'
 export class AccountMongoRepository implements IAddAccountRepository {
     async add(accountData: IAddAccountModel): Promise<IAccountModel> {
         const accountCollection = await MongoHelper.getCollection('accounts')
-        const result = await accountCollection.insertOne(123)
+        const result = await accountCollection.insertOne(accountData)
         const account = await MongoHelper.getCollectionItemById('accounts', result.insertedId)
         return MongoHelper.map(account)
     }
