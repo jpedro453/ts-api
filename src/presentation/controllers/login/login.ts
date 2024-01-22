b3d32b5b814549d3a987ed8c09127121306a4aa2
jpedro453 <jpedroswc453@gmail.com>
2024-01-22 18:03:13 -0300

    feat: ensure EmailValidator is called with correct email

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,8 +1,15 @@
 import { MissingParamError } from '../../errors'
 import { badRequest } from '../../helpers/http-helper'
 import { IController, IHttpRequest, IHttpResponse } from '../../protocols'
+import { IEmailValidator } from '../../protocols/email-validator'
 
 export class LoginController implements IController {
+    private readonly emailValidator: IEmailValidator
+
+    constructor(emailValidator: IEmailValidator) {
+        this.emailValidator = emailValidator
+    }
+
     handle(httpRequest: IHttpRequest): Promise<IHttpResponse> {
         if (!httpRequest.body.email) {
             return new Promise((resolve) => resolve(badRequest(new MissingParamError('email'))))
@@ -10,5 +17,7 @@ export class LoginController implements IController {
         if (!httpRequest.body.password) {
             return new Promise((resolve) => resolve(badRequest(new MissingParamError('password'))))
         }
+
+        const emailIsValid = this.emailValidator.isValid(httpRequest.body.email)
     }
 }
