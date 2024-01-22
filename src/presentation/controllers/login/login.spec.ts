8e5c96abd9847976cdb6b09062489e88e9379b5d
jpedro453 <jpedroswc453@gmail.com>
2024-01-22 18:43:52 -0300

    refactor: group login protocols

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -1,8 +1,7 @@
 import { LoginController } from './login'
 import { badRequest, serverError, unauthorized } from '../../helpers/http-helper'
 import { InvalidParamError, MissingParamError } from '../../errors'
-import { IEmailValidator, IHttpRequest } from '../signUp/signup-protocols'
-import { IAuthentication } from '../../../domain/useCases/authentication'
+import { IEmailValidator, IHttpRequest, IAuthentication } from './login-protocols'
 
 const makeEmailValidator = (): IEmailValidator => {
     class EmailValidatorStub implements IEmailValidator {
--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,8 +1,6 @@
-import { IAuthentication } from '../../../domain/useCases/authentication'
+import { IController, IHttpRequest, IHttpResponse, IEmailValidator, IAuthentication } from './login-protocols'
 import { InvalidParamError, MissingParamError } from '../../errors'
 import { badRequest, ok, serverError, unauthorized } from '../../helpers/http-helper'
-import { IController, IHttpRequest, IHttpResponse } from '../../protocols'
-import { IEmailValidator } from '../../protocols/email-validator'
 
 export class LoginController implements IController {
     private readonly emailValidator: IEmailValidator
