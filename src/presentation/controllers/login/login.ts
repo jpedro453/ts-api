d9c4ba1b9b7933bf46aeabb926d82e6ef1f8cef7
jpedro453 <jpedroswc453@gmail.com>
2024-01-22 18:22:15 -0300

    feat: ensure controller returns 500 if emailvalidator throws

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,5 +1,5 @@
 import { InvalidParamError, MissingParamError } from '../../errors'
-import { badRequest } from '../../helpers/http-helper'
+import { badRequest, ok, serverError } from '../../helpers/http-helper'
 import { IController, IHttpRequest, IHttpResponse } from '../../protocols'
 import { IEmailValidator } from '../../protocols/email-validator'
 
@@ -10,20 +10,24 @@ export class LoginController implements IController {
         this.emailValidator = emailValidator
     }
 
-    handle(httpRequest: IHttpRequest): Promise<IHttpResponse> {
-        const { email, password } = httpRequest.body
+    async handle(httpRequest: IHttpRequest): Promise<IHttpResponse> {
+        try {
+            const { email, password } = httpRequest.body
 
-        if (!email) {
-            return new Promise((resolve) => resolve(badRequest(new MissingParamError('email'))))
-        }
-        if (!password) {
-            return new Promise((resolve) => resolve(badRequest(new MissingParamError('password'))))
-        }
+            if (!email) {
+                return new Promise((resolve) => resolve(badRequest(new MissingParamError('email'))))
+            }
+            if (!password) {
+                return new Promise((resolve) => resolve(badRequest(new MissingParamError('password'))))
+            }
 
-        const emailIsValid = this.emailValidator.isValid(email)
+            const emailIsValid = this.emailValidator.isValid(email)
 
-        if (!emailIsValid) {
-            return new Promise((resolve) => resolve(badRequest(new InvalidParamError('email'))))
+            if (!emailIsValid) {
+                return new Promise((resolve) => resolve(badRequest(new InvalidParamError('email'))))
+            }
+        } catch (error) {
+            return serverError(error)
         }
     }
 }
