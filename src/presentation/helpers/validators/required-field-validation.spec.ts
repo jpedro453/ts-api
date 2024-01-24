6f81c4680d96863a5e9725086a5e38c729447f23
jpedro453 <jpedroswc453@gmail.com>
2024-01-24 18:36:44 -0300

    refactor: moved RequiredField sut to a factory

--- src/presentation/helpers/validators/required-field-validation.spec.ts
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -1,14 +1,19 @@
 import { MissingParamError } from '../../errors'
 import { RequiredFieldValidation } from './required-field-validation'
 
+const makeSut = (): RequiredFieldValidation => {
+    const sut = new RequiredFieldValidation('field')
+    return sut
+}
+
 describe('Required Field Validation', () => {
     test('Should return a MissingParamError if validation fails', () => {
-        const sut = new RequiredFieldValidation('field')
+        const sut = makeSut()
         const error = sut.validate({ name: 'any_name' })
         expect(error).toEqual(new MissingParamError('field'))
     })
     test('Should not return a MissingParamError if validation succeeds', () => {
-        const sut = new RequiredFieldValidation('field')
+        const sut = makeSut()
         const error = sut.validate({ field: 'any_name' })
         expect(error).toBeFalsy()
     })
