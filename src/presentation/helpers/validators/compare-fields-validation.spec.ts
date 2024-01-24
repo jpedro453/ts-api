b88c0220e1621bad2dbb1fe09a6eb9813f801c8c
jpedro453 <jpedroswc453@gmail.com>
2024-01-24 18:39:48 -0300

    test: CompareFieldsValidation tests

---
+++ src/presentation/helpers/validators/compare-fields-validation.spec.ts
@@ -0,0 +1,20 @@
+import { InvalidParamError, MissingParamError } from '../../errors'
+import { CompareFieldsValidation } from './compare-fields-validation'
+
+const makeSut = (): CompareFieldsValidation => {
+    const sut = new CompareFieldsValidation('field', 'fieldToCompare')
+    return sut
+}
+
+describe('Compare Fields Validation', () => {
+    test('Should return a InvalidParamError if validation fails', () => {
+        const sut = makeSut()
+        const error = sut.validate({ field: 'any_name', fieldToCompare: 'false_name' })
+        expect(error).toEqual(new InvalidParamError('fieldToCompare'))
+    })
+    test('Should not return a InvalidParamError if validation succeeds', () => {
+        const sut = makeSut()
+        const error = sut.validate({ field: 'any_name', fieldToCompare: 'any_name' })
+        expect(error).toBeFalsy()
+    })
+})
