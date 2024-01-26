95ce5261fd4a609b891ebfdbf49066b67fe656b6
jpedro453 <jpedroswc453@gmail.com>
2024-01-25 21:54:57 -0300

    test: ensure dbAuthentication return token on success

--- src/data/useCases/authentication/db-authentication.spec.ts
+++ src/data/useCases/authentication/db-authentication.spec.ts
@@ -121,4 +121,10 @@ describe('DB Authentication Use Case', () => {
         const promise = sut.auth(makeFakeAuthentication())
         await expect(promise).rejects.toThrow()
     })
+
+    test('Should return token on success', async () => {
+        const { sut } = makeSut()
+        const accessToken = await sut.auth(makeFakeAuthentication())
+        expect(accessToken).toBe('any_token')
+    })
 })
