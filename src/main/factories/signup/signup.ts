d92bd5becd48718ea416751276f28965370f6211
jpedro453 <jpedroswc453@gmail.com>
2024-01-29 11:54:44 -0300

    refactor: change db/mongodb folder a files names

--- src/main/factories/signup/signup.ts
+++ src/main/factories/signup/signup.ts
@@ -1,7 +1,7 @@
 import { SignUpController } from '../../../presentation/controllers/signUp/signup'
 import { DbAddAccount } from '../../../data/useCases/add-account/db-add-acount'
 import { BcryptAdapter } from '../../../infra/cryptography/bcrypt-adapter/bcrypt-adapter'
-import { AccountMongoRepository } from '../../../infra/db/mongodb/account-repository/account'
+import { AccountMongoRepository } from '../../../infra/db/mongodb/account/account-mongo-repository'
 import { IController } from '../../../presentation/protocols'
 import { LogControllerDecorator } from '../../decorators/log'
 import { LogMongoRepository } from '../../../infra/db/mongodb/log-repository/log'
