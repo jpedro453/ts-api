e78212e44697ab5c388d914f349a47c12cf47664
jpedro453 <jpedroswc453@gmail.com>
2024-01-29 12:37:21 -0300

    feat: add login factory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,5 @@
 export default {
     mongoUrl: process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/node-api',
-    port: 3030
+    port: 3030,
+    jwtSecret: process.env.JWT_SECRET || '@#A8vB2i(@'
 }
