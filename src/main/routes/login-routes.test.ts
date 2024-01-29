64e62d0f64d004b7ec7deb8d17d8f093235e9d03
jpedro453 <jpedroswc453@gmail.com>
2024-01-29 12:46:18 -0300

    test: ensure login route return 401 if login fails

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -47,5 +47,14 @@ describe('Login routes', () => {
                 })
                 .expect(200)
         })
+        test('Should return 401 on login fail', async () => {
+            await request(app)
+                .post('/api/login')
+                .send({
+                    email: 'joao@gmail.com',
+                    password: '123'
+                })
+                .expect(401)
+        })
     })
 })
