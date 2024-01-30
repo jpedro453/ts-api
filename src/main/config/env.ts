67cdf4b21f464fec35fe9edcb4a6954a24e74b40
jpedro453 <jpedroswc453@gmail.com>
2024-01-30 17:19:14 -0300

    chore: docker compose

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-    mongoUrl: process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/node-api',
+    mongoUrl: process.env.MONGO_URL || 'mongodb://mongo:27017/node-api',
     port: 3030,
     jwtSecret: process.env.JWT_SECRET || '@#A8vB2i(@'
 }
