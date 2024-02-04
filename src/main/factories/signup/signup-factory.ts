da73a46647f89ca2c5e9c1d69083c67252b274d6
jpedro453 <jpedroswc453@gmail.com>
2024-02-03 21:56:04 -0300

    refactor: required params on SignUpController factory

--- src/main/factories/signup/signup-factory.ts
+++ src/main/factories/signup/signup-factory.ts
@@ -6,14 +6,24 @@ import { IController } from '../../../presentation/protocols'
 import { LogControllerDecorator } from '../../decorators/log-controller-decorator'
 import { LogMongoRepository } from '../../../infra/db/mongodb/log/log-mongo-repository'
 import { makeSignUpValidation } from './signup-validation-factory'
+import { JwtAdapter } from '../../../infra/cryptography/jwt-adapter/jwt-adapter'
+import { DbAuthentication } from '../../../data/useCases/authentication/db-authentication-protocols'
+import env from '../../config/env'
 
 export const makeSignUpController = (): IController => {
     const salt = 12
     const bcryptAdapter = new BcryptAdapter(salt)
     const accountMongoRepository = new AccountMongoRepository()
+    const jwtAdapter = new JwtAdapter(env.jwtSecret)
+    const dbAuthentication = new DbAuthentication(
+        accountMongoRepository,
+        bcryptAdapter,
+        jwtAdapter,
+        accountMongoRepository
+    )
     const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
     const logErrorRepository = new LogMongoRepository()
-    const signUpController = new SignUpController(dbAddAccount, makeSignUpValidation())
+    const signUpController = new SignUpController(dbAddAccount, makeSignUpValidation(), dbAuthentication)
 
     return new LogControllerDecorator(signUpController, logErrorRepository)
 }
