114cd6f5300ea2a35be83baf23334bafd735d503
jpedro453 <jpedroswc453@gmail.com>
2024-02-03 21:11:54 -0300

    refactor: remove test line

--- src/presentation/controllers/signUp/signup-controller.ts
+++ src/presentation/controllers/signUp/signup-controller.ts
@@ -33,7 +33,6 @@ export class SignUpController implements IController {
                 email,
                 password
             })
-            account.name = 'TESTE'
 
             return ok(account)
         } catch (error) {
