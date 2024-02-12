13b4cd69853d340d9bb3cc842ae478d9fd3b395a
jpedro453 <jpedroswc453@gmail.com>
2024-02-12 13:34:32 -0300

    feat: ensure AddSurveyController calls AddSurvey with correct values

---
+++ src/domain/useCases/add-survey.ts
@@ -0,0 +1,12 @@
+export interface IAddSurveyModel {
+    question: string
+    answers: ISurveyAnswer[]
+}
+export interface ISurveyAnswer {
+    image?: string
+    answer: string
+}
+
+export interface IAddSurvey {
+    add(data: IAddSurveyModel): Promise<void>
+}
