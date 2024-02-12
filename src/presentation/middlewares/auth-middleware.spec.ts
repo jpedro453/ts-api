d263616c51ceb480a15d11227a213b16eb5663aa
jpedro453 <jpedroswc453@gmail.com>
2024-02-12 17:09:03 -0300

    test: ensure AuthMiddleware return 403 if no x-access-token exists in header

---
+++ src/presentation/middlewares/auth-middleware.spec.ts
@@ -0,0 +1,12 @@
+import { IHttpRequest } from '../protocols'
+import { forbidden } from '../helpers/http/http-helper'
+import { AccessDeniedError } from '../errors'
+import { AuthMiddleware } from './auth-middleware'
+
+describe('Auth Middleware', () => {
+    test('Should return 403 if no x-access-token exists in header', async () => {
+        const sut = new AuthMiddleware()
+        const httpResponse = await sut.handle({})
+        expect(httpResponse).toEqual(forbidden(new AccessDeniedError()))
+    })
+})
