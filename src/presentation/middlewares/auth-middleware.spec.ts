6334a9938ba9369c142c64f2e6c28713ccb5cf86
jpedro453 <jpedroswc453@gmail.com>
2024-02-12 18:27:00 -0300

    test: ensure AuthMiddleware calls LoadAccountByToken with correct role

--- src/presentation/middlewares/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth-middleware.spec.ts
@@ -31,9 +31,9 @@ interface ISutTypes {
     loadAccountByTokenStub: ILoadAccountByToken
 }
 
-const makeSut = (): ISutTypes => {
+const makeSut = (role?: string): ISutTypes => {
     const loadAccountByTokenStub = makeLoadAccountByTokenStub()
-    const sut = new AuthMiddleware(loadAccountByTokenStub)
+    const sut = new AuthMiddleware(loadAccountByTokenStub, role)
     return { sut, loadAccountByTokenStub }
 }
 
@@ -45,10 +45,11 @@ describe('Auth Middleware', () => {
     })
 
     test('Should call LoadAccountByToken with correct accessToken', async () => {
-        const { sut, loadAccountByTokenStub } = makeSut()
+        const role = 'any_role'
+        const { sut, loadAccountByTokenStub } = makeSut(role)
         const loadSpy = jest.spyOn(loadAccountByTokenStub, 'load')
         await sut.handle(makeFakeRequest())
-        expect(loadSpy).toHaveBeenCalledWith('any_token')
+        expect(loadSpy).toHaveBeenCalledWith('any_token', role)
     })
 
     test('Should return 403 if LoadAccountByToken returns null', async () => {
