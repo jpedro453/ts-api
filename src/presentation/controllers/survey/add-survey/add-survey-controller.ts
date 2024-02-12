c25f053324137b413e3d1d8ed602bbc60d64e41a
jpedro453 <jpedroswc453@gmail.com>
2024-02-12 13:56:58 -0300

    feat: ensure AddSurveyController returns 500 if AddSurvey throws

--- src/presentation/controllers/survey/add-survey/add-survey-controller.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.ts
@@ -1,19 +1,23 @@
-import { badRequest } from '../../../helpers/http/http-helper'
+import { badRequest, serverError } from '../../../helpers/http/http-helper'
 import { IAddSurvey, IController, IHttpRequest, IHttpResponse, IValidation } from './add-survey-controller-protocols'
 
 export class AddSurveyController implements IController {
     constructor(private readonly validation: IValidation, private readonly addSurvey: IAddSurvey) {}
 
     async handle(httpRequest: IHttpRequest): Promise<IHttpResponse> {
-        const error = this.validation.validate(httpRequest.body)
-        if (error) {
-            return badRequest(error)
+        try {
+            const error = this.validation.validate(httpRequest.body)
+            if (error) {
+                return badRequest(error)
+            }
+            const { question, answers } = httpRequest.body
+            await this.addSurvey.add({
+                question,
+                answers
+            })
+            return null
+        } catch (error) {
+            return serverError(error)
         }
-        const { question, answers } = httpRequest.body
-        await this.addSurvey.add({
-            question,
-            answers
-        })
-        return null
     }
 }
