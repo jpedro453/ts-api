c24c874833161b3f27a129091f7d93fc1506dcfd
jpedro453 <jpedroswc453@gmail.com>
2024-02-12 14:55:38 -0300

    test: ensure SurveyMongoRepository add an survey on success

---
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -0,0 +1,41 @@
+import { Collection } from 'mongodb'
+import { MongoHelper } from '../helpers/mongo-helper'
+import { SurveyMongoRepository } from './survey-mongo-repository'
+
+let surveyCollection: Collection
+
+describe('Survey Mongo Repository', () => {
+    beforeAll(async () => {
+        await MongoHelper.connect('mongodb://127.0.0.1:27017/node-api')
+    }, 5000)
+    afterAll(async () => {
+        await MongoHelper.disconnect()
+    })
+
+    beforeEach(async () => {
+        surveyCollection = await MongoHelper.getCollection('surveys')
+        await surveyCollection.deleteMany({})
+    })
+
+    const makeSut = (): SurveyMongoRepository => {
+        return new SurveyMongoRepository()
+    }
+
+    test('Should add an survey on success', async () => {
+        const sut = makeSut()
+        await sut.add({
+            question: 'any_question',
+            answers: [
+                {
+                    image: 'any_image',
+                    answer: 'any_answer'
+                },
+                {
+                    answer: 'other_answer'
+                }
+            ]
+        })
+        const survey = await surveyCollection.findOne({ question: 'any_question' })
+        expect(survey).toBeTruthy()
+    })
+})
