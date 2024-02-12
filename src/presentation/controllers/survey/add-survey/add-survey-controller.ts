65441d1b3ff71deae63de1a5f0a68f9459f0fd56
jpedro453 <jpedroswc453@gmail.com>
2024-02-12 13:10:45 -0300

    feat: ensure AddSurveyController calls Validation with correct values

---
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.ts
@@ -0,0 +1,10 @@
+import { IController, IHttpRequest, IHttpResponse, IValidation } from './add-survey-controller-protocols'
+
+export class AddSurveyController implements IController {
+    constructor(private readonly validation: IValidation) {}
+
+    handle(httpRequest: IHttpRequest): Promise<IHttpResponse> {
+        this.validation.validate(httpRequest.body)
+        return new Promise((resolve) => resolve(null))
+    }
+}
