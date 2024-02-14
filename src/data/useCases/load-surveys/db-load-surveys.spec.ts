5b622919c4073cb39019476130aa9c625d088549
jpedro453 <jpedroswc453@gmail.com>
2024-02-14 18:22:39 -0300

    test: ensure DbLoadSurveys throw if LoadSurveysRepository throws

--- src/data/useCases/load-surveys/db-load-surveys.spec.ts
+++ src/data/useCases/load-surveys/db-load-surveys.spec.ts
@@ -49,4 +49,13 @@ describe('LoadSurveys UseCase', () => {
         const surveys = await sut.load()
         expect(surveys).toEqual(makeFakeSurveys())
     })
+
+    test('Should throw if LoadSurveysRepository throws', async () => {
+        const { sut, loadSurveysRepositoryStub } = makeSut()
+        jest.spyOn(loadSurveysRepositoryStub, 'loadAll').mockReturnValueOnce(
+            new Promise((resolve, reject) => reject(new Error()))
+        )
+        const promise = sut.load()
+        expect(promise).rejects.toThrow()
+    })
 })
