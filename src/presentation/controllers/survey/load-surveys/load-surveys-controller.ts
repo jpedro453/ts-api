3192d6dd23dbc07f8de685f2be7d752dc69ec34c
jpedro453 <jpedroswc453@gmail.com>
2024-02-14 18:00:06 -0300

    feat: ensure LoadSurveysController returns 500 if LoadSurveys throws

--- src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
+++ src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
@@ -1,11 +1,15 @@
-import { ok } from '../../../helpers/http/http-helper'
+import { ok, serverError } from '../../../helpers/http/http-helper'
 import { IController, IHttpRequest, IHttpResponse, ILoadSurveys } from './load-surveys.protocols'
 
 export class LoadSurveysController implements IController {
     constructor(private readonly loadSurveys: ILoadSurveys) {}
 
     async handle(httpRequest: IHttpRequest): Promise<IHttpResponse> {
-        const surveys = await this.loadSurveys.load()
-        return ok(surveys)
+        try {
+            const surveys = await this.loadSurveys.load()
+            return ok(surveys)
+        } catch (error) {
+            return serverError(error)
+        }
     }
 }
