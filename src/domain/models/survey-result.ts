f3a21fb8140fa7ca8bddb6fa40563c70bccf634c
jpedro453 <jpedroswc453@gmail.com>
2024-02-19 18:23:27 -0300

    feat: Survey interfaces

--- src/domain/models/survey-result.ts
+++ src/domain/models/survey-result.ts
@@ -1,5 +1,7 @@
 export interface ISurveyResultModel {
-    // mongoDB ObjectID doesn't accept string, will change do Types.ObjectId later from mongoose
+    // FIXME:
+    // mongoDB ObjectID doesn't accept string, will change do Types.ObjectId later from mongoose:
+    // https://www.designcise.com/web/tutorial/which-type-to-use-for-mongoose-objectid-in-a-typescript-interface
     id: string | any
     survey_id: string | any
     account_id: string | any
