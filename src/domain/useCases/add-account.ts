0690934560913347cf570a3b3694bc3651099d07
jpedro453 <jpedroswc453@gmail.com>
2024-02-19 18:05:55 -0300

    refactor: now IAddAccountModel is a type omiting de IAccountModel id

--- src/domain/useCases/add-account.ts
+++ src/domain/useCases/add-account.ts
@@ -1,10 +1,6 @@
 import { IAccountModel } from '@/domain/models/account'
 
-export interface IAddAccountModel {
-    name: string
-    email: string
-    password: string
-}
+export type IAddAccountModel = Omit<IAccountModel, 'id'>
 
 export interface IAddAccount {
     add(account: IAddAccountModel): Promise<IAccountModel>
