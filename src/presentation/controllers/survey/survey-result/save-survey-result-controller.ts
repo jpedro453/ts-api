b800409c30c16a7d4fbd1ee612b008c97ea16675
jpedro453 <jpedroswc453@gmail.com>
2024-02-22 17:47:15 -0300

    feat: ensure SaveSurveyResultController returns 403 if loadSurveyById returns null

--- src/presentation/controllers/survey/survey-result/save-survey-result-controller.ts
+++ src/presentation/controllers/survey/survey-result/save-survey-result-controller.ts
@@ -1,10 +1,15 @@
 import { ILoadSurveyById } from '@/domain/useCases/survey/load-survey-by-id'
 import { IController, IHttpRequest, IHttpResponse } from '../add-survey/add-survey-controller-protocols'
+import { forbidden } from '@/presentation/helpers/http/http-helper'
+import { InvalidParamError } from '@/presentation/errors'
 
 export class SaveSurveyResultController implements IController {
     constructor(private readonly loadSurveyById: ILoadSurveyById) {}
     async handle(httpRequest: IHttpRequest): Promise<IHttpResponse> {
-        await this.loadSurveyById.loadById(httpRequest.params.surveyId)
+        const survey = await this.loadSurveyById.loadById(httpRequest.params.surveyId)
+        if (!survey) {
+            return forbidden(new InvalidParamError('SurveyID'))
+        }
         return null
     }
 }
