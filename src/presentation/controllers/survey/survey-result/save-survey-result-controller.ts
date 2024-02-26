690ab6bcf2232402059106285642d1ce21983bec
jpedro453 <jpedroswc453@gmail.com>
2024-02-25 22:07:05 -0300

    feat: ensure SaveSurveyResult Controller calls SaveSurveyResult with correct values

--- src/presentation/controllers/survey/survey-result/save-survey-result-controller.ts
+++ src/presentation/controllers/survey/survey-result/save-survey-result-controller.ts
@@ -2,14 +2,20 @@ import { ILoadSurveyById } from '@/domain/useCases/survey/load-survey-by-id'
 import { IController, IHttpRequest, IHttpResponse } from '../add-survey/add-survey-controller-protocols'
 import { forbidden, serverError } from '@/presentation/helpers/http/http-helper'
 import { InvalidParamError } from '@/presentation/errors'
+import { ISaveSurveyResult } from '@/domain/useCases/survey/save-survey-result'
 
 export class SaveSurveyResultController implements IController {
-    constructor(private readonly loadSurveyById: ILoadSurveyById) {}
+    constructor(
+        private readonly loadSurveyById: ILoadSurveyById,
+        private readonly saveSurveyResult: ISaveSurveyResult
+    ) {}
     async handle(httpRequest: IHttpRequest): Promise<IHttpResponse> {
         try {
-            const { surveyId } = httpRequest.params
+            const { survey_id } = httpRequest.params
             const { answer } = httpRequest.body
-            const survey = await this.loadSurveyById.loadById(surveyId)
+            const { account_id } = httpRequest
+
+            const survey = await this.loadSurveyById.loadById(survey_id)
 
             if (survey) {
                 const answers = survey.answers.map((a) => a.answer)
@@ -20,6 +26,13 @@ export class SaveSurveyResultController implements IController {
             } else {
                 return forbidden(new InvalidParamError('SurveyID'))
             }
+            await this.saveSurveyResult.save({
+                survey_id,
+                account_id,
+                answer,
+                date: new Date()
+            })
+
             return null
         } catch (error) {
             return serverError(error)
