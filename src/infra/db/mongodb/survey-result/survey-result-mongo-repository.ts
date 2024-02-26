606491561119ecf9d6359ad1ff56e2667ed52a0b
jpedro453 <jpedroswc453@gmail.com>
2024-02-25 21:10:31 -0300

    feat: ensure SurveyResultMongoRepository add an survey result if there is no data with account_id or survey_id

---
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-repository.ts
@@ -0,0 +1,27 @@
+import { ISaveSurveyResultRepository } from '@/data/protocols/db/survey/save-survey-result-repository'
+import { ISurveyResultModel } from '@/domain/models/survey-result'
+import { ISaveSurveyResultModel } from '@/domain/useCases/survey/save-survey-result'
+import { MongoHelper } from '../helpers/mongo-helper'
+
+export class SurveyResultMongoRepository {
+    async save(data: ISaveSurveyResultModel): Promise<ISurveyResultModel> {
+        const surveyResultCollection = await MongoHelper.getCollection('surveyResults')
+        const res = await surveyResultCollection.findOneAndUpdate(
+            {
+                survey_id: data.survey_id,
+                account_id: data.account_id
+            },
+            {
+                $set: {
+                    answer: data.answer,
+                    date: data.date
+                }
+            },
+            {
+                upsert: true,
+                returnDocument: 'after'
+            }
+        )
+        return MongoHelper.map(res)
+    }
+}
