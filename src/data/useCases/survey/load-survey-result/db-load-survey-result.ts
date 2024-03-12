a5e9f58c68daed9d26f501d08cfe109b66794ac8
jpedro453 <jpedroswc453@gmail.com>
2024-03-12 18:38:56 -0300

    feat: ensure DbLoadSurveyResult calls LoadSurveyResultRepository with correct id

---
+++ src/data/useCases/survey/load-survey-result/db-load-survey-result.ts
@@ -0,0 +1,12 @@
+import { ILoadSurveyResultRepository } from '@/data/protocols/db/survey/load-survey-result-repository'
+import { ISurveyResultModel } from '@/domain/models/survey-result'
+import { ILoadSurveyResult } from '@/domain/useCases/survey/load-survey-result'
+
+export class DbLoadSurveyResult implements ILoadSurveyResult {
+    constructor(private readonly loadSurveyResultRepository: ILoadSurveyResultRepository) {}
+
+    async load(survey_id: string): Promise<ISurveyResultModel> {
+        await this.loadSurveyResultRepository.loadBySurveyId(survey_id)
+        return Promise.resolve(null)
+    }
+}
