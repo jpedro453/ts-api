e1a3f700d207e665d276b90eae7c30dab962ab07
jpedro453 <jpedroswc453@gmail.com>
2024-03-14 16:38:29 -0300

    refactor: ensure DbLoadSurveyResult calls LoadSurveyByIdRepository if loadSurveyResultRepository returns null

--- src/data/useCases/survey/load-survey-result/db-load-survey-result.ts
+++ src/data/useCases/survey/load-survey-result/db-load-survey-result.ts
@@ -1,12 +1,19 @@
+import { ILoadSurveyByIdRepository } from '@/data/protocols/db/survey/load-survey-by-id-repository'
 import { ILoadSurveyResultRepository } from '@/data/protocols/db/survey/load-survey-result-repository'
 import { ISurveyResultModel } from '@/domain/models/survey-result'
 import { ILoadSurveyResult } from '@/domain/useCases/survey/load-survey-result'
 
 export class DbLoadSurveyResult implements ILoadSurveyResult {
-    constructor(private readonly loadSurveyResultRepository: ILoadSurveyResultRepository) {}
+    constructor(
+        private readonly loadSurveyResultRepository: ILoadSurveyResultRepository,
+        private readonly loadSurveyByIdRepository: ILoadSurveyByIdRepository
+    ) {}
 
     async load(survey_id: string): Promise<ISurveyResultModel> {
         const surveyResult = await this.loadSurveyResultRepository.loadBySurveyId(survey_id)
+        if (!surveyResult) {
+            await this.loadSurveyByIdRepository.loadById(survey_id)
+        }
         return surveyResult
     }
 }
