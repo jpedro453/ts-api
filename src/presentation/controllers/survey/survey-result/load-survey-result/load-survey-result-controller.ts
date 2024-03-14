87f28eb1e605b2213be15f2d3de714af4f662e5e
jpedro453 <jpedroswc453@gmail.com>
2024-03-14 17:07:38 -0300

    test: ensure LoadSurveyResultController returns 403 if LoadSurveyById returns null

--- src/presentation/controllers/survey/survey-result/load-survey-result/load-survey-result-controller.ts
+++ src/presentation/controllers/survey/survey-result/load-survey-result/load-survey-result-controller.ts
@@ -1,12 +1,17 @@
 import { ILoadSurveyById } from '@/domain/useCases/survey/load-survey-by-id'
 import { IController, IHttpRequest, IHttpResponse } from '../../add-survey/add-survey-controller-protocols'
+import { forbidden } from '@/presentation/helpers/http/http-helper'
+import { InvalidParamError } from '@/presentation/errors'
 
 export class LoadSurveyResultController implements IController {
     constructor(private readonly loadSurveyById: ILoadSurveyById) {}
 
     async handle(httpRequest: IHttpRequest): Promise<IHttpResponse> {
         const { survey_id } = httpRequest.params
-        await this.loadSurveyById.loadById(survey_id)
+        const survey = await this.loadSurveyById.loadById(survey_id)
+        if (!survey) {
+            return forbidden(new InvalidParamError('survey_id'))
+        }
         return null
     }
 }
