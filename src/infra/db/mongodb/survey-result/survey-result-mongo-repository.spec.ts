ec4604cb9591b2e70283e89f09bdb1ef671e1212
jpedro453 <jpedroswc453@gmail.com>
2024-03-15 18:36:33 -0300

    test: ensure Survey Result Mongo Repository returns null if there is no survey result

--- src/infra/db/mongodb/survey-result/survey-result-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-repository.spec.ts
@@ -168,5 +168,11 @@ describe('Survey Result Mongo Repository', () => {
             expect(surveyResult.answers[2].percent).toBe(0)
             expect(surveyResult.answers[2].percent).toBe(0)
         })
+        test('should return null if there is no survey result', async () => {
+            const survey = await makeSurvey()
+            const sut = makeSut()
+            const surveyResult = await sut.loadBySurveyId(survey.id)
+            expect(surveyResult).toBeNull()
+        })
     })
 })
